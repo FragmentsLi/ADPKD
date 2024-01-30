6998333e2f5ec74cf41e03b6c18f10a07c41f36b
FragmentsLi <127385089+FragmentsLi@users.noreply.github.com>
2024-01-30 15:36:44 +0800

    Update Tcell.R

--- Tcell.R
+++ Tcell.R
@@ -1,31 +1,49 @@
+####################################################################
+#   1. Importing Data
+####################################################################
 library(Seurat)
 library(ggplot2)
 load('adpkd_all.RData')
 
-MNP <- subset(adpkd_qc,idents = c("MNP"))
-colnames(MNP@meta.data)[5]<-'CellType.0.7'
+Tcell <- subset(adpkd_qc,idents = c("Tcell"))
+colnames(Tcell@meta.data)[5]<-'CellType.0.7'
 
 ####################################################################
 #   2. Preprocessing
 ####################################################################
-MNP <- NormalizeData(MNP)
+Tcell <- NormalizeData(Tcell)
 set.seed(1218)
-MNP <- FindVariableFeatures(MNP, selection.method = "vst", nfeatures = 2000)
-MNP <- ScaleData(MNP, features = row.names(MNP),verbose = FALSE)
+Tcell <- FindVariableFeatures(Tcell, selection.method = "vst", nfeatures = 2000)
+Tcell <- ScaleData(Tcell, features = row.names(Tcell),verbose = FALSE)
 set.seed(1218)
-MNP <- RunPCA(MNP, pc.genes = VariableFeatures(object = MNP))
+Tcell <- RunPCA(Tcell, pc.genes = VariableFeatures(object = Tcell))
 
 #  harmony
 library(harmony)
 set.seed(1218)
-MNP <- RunHarmony(MNP,"batch", plot_convergence = TRUE)
-MNP <- RunUMAP(MNP, reduction = "harmony", dims = 1:20)
-MNP <- FindNeighbors(MNP, reduction = "harmony", dims = 1:20)
-MNP <- RunTSNE(MNP,dims=1:20)
+Tcell <- RunHarmony(Tcell,"batch", plot_convergence = TRUE)
+Tcell <- RunUMAP(Tcell, reduction = "harmony", dims = 1:20)
+Tcell <- FindNeighbors(Tcell, reduction = "harmony", dims = 1:20)
+
 library(clustree)
 set.seed(1218)
-MNP <- FindClusters(
-  object = MNP,
+Tcell <- FindClusters(
+  object = Tcell,
   resolution = c(seq(.1,1.6,.2))
 )
+
+####################################################################
+#   3. Annotation
+####################################################################
+Idents(Tcell) <- 'RNA_snn_res.0.1'
+#remove cluster 7
+Tcell<-subset(Tcell,idents=c(0:6,8))
+type<-c('CD4+IL7R+ C1','CD8+NKG7+ C1','CD8+CCL4L2+ C2','CD4+TNF+ C2','CD8+MGP+ C3',#0,1,2,3,4
+        'NK','CD4+RTKN2+ C3','CD8+STMN1+ C4')#5,6,8
+names(type) <- levels(Tcell) 
+Tcell <- RenameIdents(Tcell, type)
+Idents(Tcell)<-ordered(Idents(Tcell),
+                       levels=c('CD4+IL7R+ C1','CD4+TNF+ C2','CD4+RTKN2+ C3',
+                                'CD8+NKG7+ C1','CD8+CCL4L2+ C2','CD8+MGP+ C3','CD8+STMN1+ C4','NK'))
+Tcell$subtype <- Idents(Tcell)
 
