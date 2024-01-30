9e4bbf5cf8eb0e8bf3382ed4ebb9076c08546491
FragmentsLi <127385089+FragmentsLi@users.noreply.github.com>
2024-01-30 16:24:28 +0800

    Update Pathway enrichment analysis.R

--- Pathway enrichment analysis.R
+++ Pathway enrichment analysis.R
@@ -1,9 +1,8 @@
-library(VISION)
-library(Seurat)
-
 ####################################################################
 #   1. Pathway Enrichment for Major Cell Lineages
 ####################################################################
+library(VISION)
+library(Seurat)
 load('adpkd_all.RData')
 # 1)VISION
 vision.obj <- Vision(adpkd_qc,
@@ -31,35 +30,3 @@ control<-c('PT_Normal','Tcell_Normal','MNP_Normal',
         'FIB_Normal','Bcell_Normal','MAST_Normal','EPI_Normal',
         'ENDO_Normal','DT_Normal')
 sigScores<-sigScores[,c(adpkd,control)]
-
-# 2)Creating Supplementary Fig.2
-annotation_col<-data.frame(Group=factor(c(rep('ADPKD',9),rep('Normal',9))),
-                           Type=factor(rep(c('PT','Tcell','MNP',
-        'FIB','Bcell','MAST','EPI',
-        'ENDO','DT'),2)))
-rownames(annotation_col)<-colnames(sigScores)
-
-library(ggsci)
-ann_colors<-list(Group=c('ADPKD'='#f47720','Normal'='#459943'),
-                       Type=c('PT'=pal_npg()(9)[1],'Tcell'=pal_npg()(9)[2],
-                             'MNP'=pal_npg()(9)[3],'FIB'=pal_npg()(9)[4],
-                             'Bcell'=pal_npg()(9)[5],'MAST'=pal_npg()(9)[6],
-                             'EPI'=pal_npg()(9)[7],'ENDO'=pal_npg()(9)[8],
-                             'DT'=pal_npg()(9)[9]))
-
-p <- pheatmap::pheatmap(sigScores,scale = "row",cluster_cols = F,
-                        annotation_col=annotation_col,
-                        fontsize=8,
-                        border_color = "grey",
-                        cellwidth = 12, 
-                        cellheight = 12,
-                        annotation_colors=ann_colors,angle_col=90,
-                        labels_col=rep(c('PT','Tcell','MNP',
-        'FIB','Bcell','MAST','EPI',
-        'ENDO','DT'),2))
-
-png(filename = "ADPKD_VISION_hallmark_all.png",width = 230, height = 280, 
-    units = "mm", res = 300)
-p
-dev.off()
-
